68e39d6092824af321f9c941bccea9d026d2c981
jiachenning <jiachenning@gmail.com>
2014-12-14 22:47:21 +0800

    Initial Commit

---
+++ plistReader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  plistReader
+//
+//  Created by ninja chen on 14/12/14.
+//  Copyright (c) 2014年 ninja chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ plistReader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  plistReader
+//
+//  Created by ninja chen on 14/12/14.
+//  Copyright (c) 2014年 ninja chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
