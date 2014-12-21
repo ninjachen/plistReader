7d8ef731a07fef6eb8419a7f04c7d21d28b885c0
jiachenning <jiachenning@gmail.com>
2014-12-21 16:38:15 +0800

    finish 1st edition

--- plistReader/ViewController.m
+++ plistReader/ViewController.m
@@ -16,7 +16,30 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    NSString *plistPath = [[NSBundle mainBundle] pathForResource:@"areaNumber" ofType:@"plist"];
+    NSArray *areaArray = [[NSArray alloc] initWithContentsOfFile:plistPath];
+    NSError *error = nil;
+    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:areaArray options:NSJSONWritingPrettyPrinted error:&error];
+    //NSData转换为String
+    NSString *jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+    NSLog(@"JSON String = %@", jsonString);
+    NSArray *documentPaths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory,  NSUserDomainMask,YES);
+    NSString *ourDocumentPath =[documentPaths objectAtIndex:0];
+    NSString *fileName=[ourDocumentPath stringByAppendingPathComponent:@"ninjafile"];
+    [jsonData writeToFile:fileName atomically:YES];
+    //遍历
+//    for (NSDictionary *dic in areaArray) {
+//        NSArray *citiesArray = dic[@"cities"];
+//        for (NSDictionary *shiDic in citiesArray) {
+//             NSArray *quArray = shiDic[@"areas"];
+//            for (NSDictionary *quDic in quArray) {
+//                for(id key in quDic){
+//                    NSLog(@"key %@, value %@", key, quDic[key]);
+//
+//                }
+//            }
+//        }
+//    }
 }
 
 - (void)didReceiveMemoryWarning {
